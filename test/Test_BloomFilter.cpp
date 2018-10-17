da9c490adcfd84389b56f9f236bb44a358315825
Alexander Wilson <acwilson96@gmail.com>
2018-10-17 01:01:08 +0100

    Copy Constructor & Assignment Operator Unit Tests

--- test/Test_BloomFilter.cpp
+++ test/Test_BloomFilter.cpp
@@ -26,6 +26,37 @@ TEST(BloomFilterTest, InitialisedToZeros) {
 	ASSERT_EQ("0000000000", bf.dump());
 }
 
+TEST(BloomFilterTest, CopyConstructor) {
+	bloom_filter bf(2, 0.091848839);
+
+	bf.insert("entry");
+
+	ASSERT_TRUE(bf.find("entry"));
+
+	bloom_filter bf_copy(bf);
+
+	ASSERT_TRUE(bf.find("entry"));
+	ASSERT_TRUE(bf_copy.find("entry"));
+}
+
+TEST(BloomFilterTest, AssignmentCopy) {
+	bloom_filter bf(2, 0.091848839);
+
+	bf.insert("entry");
+
+	ASSERT_TRUE(bf.find("entry"));
+
+	bloom_filter bf_copy = bf;
+
+	ASSERT_TRUE(bf.find("entry"));
+	ASSERT_TRUE(bf_copy.find("entry"));
+
+	bf_copy = bf_copy;
+
+	ASSERT_TRUE(bf.find("entry"));
+	ASSERT_TRUE(bf_copy.find("entry"));
+}
+
 TEST(BloomFilterTest, TestInsert) {
 	bloom_filter bf(125000, 0.01);
 	std::string bitsBefore = bf.dump();
